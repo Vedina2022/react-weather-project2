282b67f678479acba2f4576d0ffde11d85787c49
Vedina2022 <2014vedina@gmail.com>
2022-09-10 14:57:30 -0600

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Nadiia Kyshinska and
-        <a href="https://github.com/Vedina2022/react-weather-project2">
-          {" "}
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Nadiia Kyshinska and
+          <a href="https://github.com/Vedina2022/react-weather-project2">
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
