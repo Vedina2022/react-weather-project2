819c3ed39b71de02bb9d680dc2c806f5a1a38977
Vedina2022 <2014vedina@gmail.com>
2022-09-10 14:49:48 -0600

    Added the GitHub link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Nadiia Kyshinska and
+        <a href="https://github.com/Vedina2022/react-weather-project2">
+          {" "}
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
